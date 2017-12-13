06f9dba1d7df674fee35bd0e8a8faa4c0ef6fe02
Yekeen Ajeigbe <aayekeen@gmail.com>
2017-12-13 13:12:30 +0100

    Setup folder structure. Added router.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Fragment
+} from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Dashboard from './sections/Dashboard'
+import Login from './sections/Login'
 
-export default App;
+const App = () => (
+  <Router>
+    <Fragment>
+      <Route
+        component={Login}
+        exact
+        path='/'
+      />
+      <Route
+        component={Dashboard}
+        exact
+        path='/dashboard'
+      />
+    </Fragment>
+  </Router>
+)
+
+export default App
